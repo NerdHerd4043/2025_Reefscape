8bd0d42b9fd0905bec5c8fb0f6386f0bc553360f
Zynh Ludwig <zynh0722@gmail.com>
2025-02-16 03:11:13 -0800

    2/16 Zynh - cowlib.Util.scaledXY(double[])

--- src/main/java/cowlib/Util.java
+++ src/main/java/cowlib/Util.java
@@ -17,4 +17,36 @@ public class Util {
   public static final double mapDouble(double x, double a, double b, double c, double d) {
     return (x - a) / (b - a) * (d - c) + c;
   }
+
+  /**
+   * @param xy double array of length 2, to be squared by it's magnitude
+   *
+   *           squares a 2d vector
+   */
+  public static final double[] square2DVector(double[] xy) {
+    // I'd prefer to copy xy here into a new variable here, but I think that'd be
+    // wasteful
+    // so this method just modified the input and returns it
+
+    // Converting to Polar coordinates (uses coordinates (r, theta) where `r` is
+    // magnitude and `theta` is the angle relative to 0. Usually 0 is in the
+    // positive direction of a cartesian graph's x axis, and increases positively
+    // with counterclockwise rotation).
+    double r = Math.sqrt(xy[0] * xy[0] + xy[1] * xy[1]);
+    double theta = Math.atan2(xy[1], xy[0]);
+
+    // Square radius and scale by max velocity. This allows for slower speed when
+    // the drivestick is closer to the center without limiting the max speed because
+    // 1 is the max output of the drivestick and 1 * 1 = 1.
+    r = r * r;
+
+    // Convert to Cartesian coordinates (uses coordinates (x,y)) by getting the `x`
+    // and `y` legs of the right triangle where `r` is the hypotenuse and `x` and
+    // `y` are the legs. Learn trigonometry *shrug*. Also multiplies by 0.5 if the
+    // elevator is in use so the robot has smaller chances of tipping.
+    xy[0] = r * Math.cos(theta);
+    xy[1] = r * Math.sin(theta);
+
+    return xy;
+  }
 }
