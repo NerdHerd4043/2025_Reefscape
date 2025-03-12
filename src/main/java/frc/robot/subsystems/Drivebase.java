5f32fc8f01ded33bc4e7e4f48a34dfa3b8a78e89
Senor-Pie <espenpbock@gmail.com>
2025-03-11 23:17:19 -0700

    3/11 Espen - Switching to Target Oriented

--- src/main/java/frc/robot/subsystems/Drivebase.java
+++ src/main/java/frc/robot/subsystems/Drivebase.java
@@ -151,7 +151,7 @@ public class Drivebase extends SubsystemBase {
 
     // Getting the Limelight's field position array from network tables.
     NetworkTable LLTable = inst.getTable("limelight-right");
-    DoubleArrayTopic botPoseTopic = LLTable.getDoubleArrayTopic("botpose_orb_wpiblue");
+    DoubleArrayTopic botPoseTopic = LLTable.getDoubleArrayTopic("botpose_target_space");
     this.botFieldPose = botPoseTopic.subscribe(new double[6]);
 
     // Setting up auto capabilities
@@ -449,20 +449,12 @@ public class Drivebase extends SubsystemBase {
 
     this.field.setRobotPose(this.getRobotPose()); // Shows robot pose according to odometry
 
-    // SmartDashboard.putNumber("Speed Ratio", this.getRobotSpeedRatio()); //
-    // Displays speed ratio (not interactable)
+    SmartDashboard.putNumber("R Target", LimelightUtil.getID("limelight-right"));
 
-    // SmartDashboard.putNumber("R Target", LimelightUtil.getID("limelight-right"));
-
-    // SmartDashboard.putNumber("Field Pose X", this.botFieldPoseArray[0]); // Field
-    // Y Pose
-    // SmartDashboard.putNumber("Field Pose Y", this.botFieldPoseArray[1]); // Field
-    // Y Pose
-    // SmartDashboard.putNumber("LL Latency", this.botFieldPoseArray[5]); // Latency
-
-    // SmartDashboard.putNumber("LL Target", LimelightUtil.getID(getName()));
+    SmartDashboard.putNumber("Field Pose X", this.botFieldPoseArray[0]); // X Pose
+    SmartDashboard.putNumber("Field Pose Y", this.botFieldPoseArray[1]); // Y Pose
+    SmartDashboard.putNumber("LL Latency", this.botFieldPoseArray[5]); // Latency
 
     SmartDashboard.putNumber("Yaw", this.getFieldAngle());
-
   }
 }
